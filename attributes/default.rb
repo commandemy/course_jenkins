ba04ce21e3f43dc4df5db190e39b2c78c348ce7a
Jürgen Brüder <juergen.brueder@gmail.com>
2015-09-24 14:13:17 +0200

    also add chef-server to host file

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,6 +5,7 @@
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
 default['course_jenkins']['gitlab_ip'] = '172.24.4.133'
+default['course_jenkins']['chef_server_ip'] = '172.24.4.132'
 
 default['jenkins']['cookbooks'] = [
   {name: 'app', git: 'http://gitlab.course.com/root/course_app.git'},
